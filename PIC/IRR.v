6b4412f40afba2332078891702bd9175f5314005
Fady Milad <127983594+FadyMilad22@users.noreply.github.com>
2023-12-21 17:01:00 +0200

    edit IRR.v

--- PIC/IRR.v
+++ PIC/IRR.v
@@ -1,55 +1,66 @@
 module Interrupt_Request (
     input  wire  clock,
-    input  wire  reset,
-
+    input  wire  write_initial_command_word_1_reset,
     // Inputs from control logic
-    input  wire  level_or_edge_toriggered_config,
+    input  wire  level_or_edge_triggered_config,
     input  wire  freeze,
     input  wire  [7:0] clear_interrupt_request,
 
-    // External inputs
+    // pic input pins
     input  wire  [7:0] interrupt_request_pin,
 
     // Outputs
     output reg  [7:0] interrupt_request_register
 );
 
-    reg [7:0] low_input_latch;
+    reg  [7:0] low_input_latch;
     wire [7:0] interrupt_request_edge;
 
-    genvar ir_bit_no;
-    generate
-  for (ir_bit_no = 0; ir_bit_no < 8; ir_bit_no = ir_bit_no + 1) begin
-    always @(posedge clock or posedge reset) begin 
-      
-                if (reset) begin
+        //
+        // Edge Sense
+        //
+    integer ir_bit_no;
+    
+  
+    always @(posedge clock or posedge write_initial_command_word_1_reset) begin 
+      for (ir_bit_no = 0; ir_bit_no < 8; ir_bit_no = ir_bit_no + 1) begin
+                if (write_initial_command_word_1_reset) begin
                 low_input_latch <= 8'b0;
-                end else if (clear_interrupt_request[ir_bit_no]) begin
+                end 
+                else if (clear_interrupt_request[ir_bit_no]) begin
                     low_input_latch[ir_bit_no] <= 1'b0;
-                end else if (~interrupt_request_pin[ir_bit_no]) begin
+                end 
+                else if (~interrupt_request_pin[ir_bit_no]) begin
                     low_input_latch[ir_bit_no] <= 1'b1;
-                end else begin
+                end 
+                else begin
                     low_input_latch[ir_bit_no] <= low_input_latch[ir_bit_no];
                 end
             end
+    end
 
     assign interrupt_request_edge = low_input_latch & interrupt_request_pin;
 
-    always @(posedge clock or posedge reset) begin
-                if (reset) begin
+         //
+        // IRR
+        //
+    always @(posedge clock or posedge write_initial_command_word_1_reset) begin
+        for (ir_bit_no = 0; ir_bit_no < 8; ir_bit_no = ir_bit_no + 1) begin
+                if (write_initial_command_word_1_reset) begin
                 interrupt_request_register <= 8'b0;
-                end else if (clear_interrupt_request[ir_bit_no]) begin
+                end 
+                else if (clear_interrupt_request[ir_bit_no]) begin
                     interrupt_request_register[ir_bit_no] <= 1'b0;
-                end else if (freeze) begin
+                end 
+                else if (freeze) begin
                     interrupt_request_register[ir_bit_no] <= interrupt_request_register[ir_bit_no];
-                end else if (level_or_edge_toriggered_config) begin
+                end 
+                else if (level_or_edge_triggered_config) begin
                     interrupt_request_register[ir_bit_no] <= interrupt_request_pin[ir_bit_no];
-                end else begin
+                end 
+                else begin
                     interrupt_request_register[ir_bit_no] <= interrupt_request_edge[ir_bit_no];
                 end
-            
-        
-    end
+        end  
   end
-endgenerate
 endmodule
