1c1dd8c62979cb60d56965195136de2f0dc8bf24
Fady Milad <127983594+FadyMilad22@users.noreply.github.com>
2024-01-02 15:41:49 +0200

    Update IRR.v

--- PIC/IRR.v
+++ PIC/IRR.v
@@ -22,7 +22,7 @@ module Interrupt_Request (
     integer ir_bit_no;
     
   
-    always @(posedge clock or posedge write_initial_command_word_1_reset) begin 
+    always @(*) begin 
       for (ir_bit_no = 0; ir_bit_no < 8; ir_bit_no = ir_bit_no + 1) begin
                 if (write_initial_command_word_1_reset) begin
                 low_input_latch <= 8'b0;
@@ -44,7 +44,7 @@ module Interrupt_Request (
          //
         // IRR
         //
-    always @(posedge clock or posedge write_initial_command_word_1_reset) begin
+    always @(*) begin
         for (ir_bit_no = 0; ir_bit_no < 8; ir_bit_no = ir_bit_no + 1) begin
                 if (write_initial_command_word_1_reset) begin
                 interrupt_request_register <= 8'b0;
