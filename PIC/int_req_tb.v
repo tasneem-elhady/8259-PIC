6cc6e5eaed6c6ca8229bc80354bb2828313d9626
tasneem-elhady <127930144+tasneem-elhady@users.noreply.github.com>
2024-01-02 19:36:40 +0200

    Update int_req_tb.v

--- PIC/int_req_tb.v
+++ PIC/int_req_tb.v
@@ -1,153 +1,176 @@
-`include "int_req.v"
+`define TB_CYCLE        20
+`define TB_FINISH_COUNT 20000
 
-module int_req_tb;
+module Interrupt_Request_tb();
 
-	reg clock;
-	reg write_initial_command_word_1_reset;
-	
-	initial clock = 1'b1;
-	always #(10) clock = ~clock;
-	
-	initial begin
-		write_initial_command_word_1_reset = 1'b1;
-		#(200) write_initial_command_word_1_reset = 1'b0;
-	end
+    //
+    // Generate wave file to check
+    //
+`ifdef IVERILOG
+    initial begin
+        $dumpfile("tb.vcd");
+        $dumpvars(0, tb);
+    end
+`endif
 
-	reg [31:0] tb_cycle_counter;
+    //
+    // Generate clock
+    //
+    reg   clock;
+    initial clock = 1'b1;
+    always #(20 / 2) clock = ~clock;
 
-	always @(negedge clock)
-		if (write_initial_command_word_1_reset)
-			tb_cycle_counter <= 32'h00000000;
-		else
-			tb_cycle_counter <= tb_cycle_counter + 32'h00000001;
-	
-	always @(*) begin
-		if (tb_cycle_counter == 20000) begin
-			$display("## SIMULATION TIMEOUT ## at %d", tb_cycle_counter);
-			$finish;
-		end
-	end
-
-	reg level_toriggered_config;
-	reg freeze;
-	reg [7:0] clear_interrupt_request;
-	reg [7:0] interrupt_request_pin;
-	wire [7:0] interrupt_request_register;
-	
-	int_req u_int_req();
-	
-	task TASK_INIT;
-		begin
-			#(0)
-				;
-			level_toriggered_config = 1'b0;
-			freeze = 1'b0;
-			interrupt_request_pin = 8'b00000000;
-			clear_interrupt_request = 8'b00000000;
-			#(240)
-				;
-		end
-	endtask
-
-	task TASK_LEVEL_TRIGGER_INTERRUPT;
-		input [7:0] data;
-		begin
-			#(0)
-				;
-			level_toriggered_config = 1'b1;
-			#(20)
-				;
-			interrupt_request_pin = data;
-			#(20)
-				;
-			clear_interrupt_request = data;
-			#(20)
-				;
-			clear_interrupt_request = 8'b00000000;
-			#(20)
-				;
-		end
-	endtask
-
-	task TASK_EDGE_TRIGGER_INTERRUPT;
-		input [7:0] data;
-		begin
-			#(0)
-				;
-			level_toriggered_config = 1'b0;
-			#(20)
-				;
-			interrupt_request_pin = 8'b00000000;
-			#(20)
-				;
-			interrupt_request_pin = data;
-			#(20)
-				;
-			clear_interrupt_request = data;
-			#(20)
-				;
-			clear_interrupt_request = 8'b00000000;
-			#(20)
-				;
-		end
-	endtask
-
-	task TASK_CLEAR_INTERRUPT_REQUEST;
-		input [7:0] data;
-		begin
-			#(0)
-				;
-			clear_interrupt_request = data;
-			#(20)
-				;
-		end
-	endtask
-
-	task TASK_INTERRUPT_TEST;
-		begin
-			#(0)
-				;
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000001);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000010);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000100);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00001000);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00010000);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00100000);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b01000000);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b10000000);
-			TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000000);
-			TASK_CLEAR_INTERRUPT_REQUEST(8'b11111111);
-			TASK_CLEAR_INTERRUPT_REQUEST(8'b00000000);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b10000000);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b01000000);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b00100000);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b00010000);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b00001000);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b00000100);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b00000010);
-			TASK_EDGE_TRIGGER_INTERRUPT(8'b00000001);
-			TASK_CLEAR_INTERRUPT_REQUEST(8'b11111111);
-			TASK_CLEAR_INTERRUPT_REQUEST(8'b00000000);
-			#(20)
-				;
-		end
-	endtask
-
-	initial begin
-		TASK_INIT;
-		TASK_INTERRUPT_TEST;
-		freeze = 1'b1;
-		TASK_INTERRUPT_TEST;
-		#(20)
-			;
-		$finish;
-	end
-
-
-	initial
-    begin
-		$dumpfile("int_req_tb.vcd");
-		$dumpvars(0,int_req_tb);
+    //
+    // Generate reset
+    //
+    reg write_initial_command_word_1_reset;
+    initial begin
+        write_initial_command_word_1_reset = 1'b1;
+            # (20 * 10)
+        write_initial_command_word_1_reset = 1'b0;
     end
 
+    //
+    // Module under test
+    //
+    //
+    reg            level_triggered_config;
+    reg            freeze;
+    reg    [7:0]   clear_interrupt_request;
+    reg    [7:0]   interrupt_request_pin;
+    wire   [7:0]   interrupt_request_register;
+
+     Interrupt_Request IRR_tb(
+        //  clock,                       
+        .write_initial_command_word_1_reset     (write_initial_command_word_1_reset),                             
+        // Inputs from control logic                                
+        .level_triggered_config        (level_triggered_config),                            
+        .freeze                                (freeze),           
+        .clear_interrupt_request               (clear_interrupt_request),          
+        // pic input pins                                      
+        .interrupt_request_pin                 (interrupt_request_pin),           
+        // Outputs                               
+        .interrupt_request_register            (interrupt_request_register)                                                                  
+    );
+
+    //
+    // Task : Initialization
+    //
+    task TASK_INIT();
+    begin
+        level_triggered_config = 1'b0;
+        freeze                  = 1'b0;
+        interrupt_request_pin   = 8'b00000000;
+        clear_interrupt_request = 8'b00000000;
+        #(20);
+    end
+    endtask
+
+    //
+    // Task : Level trigger interrupt
+    //
+    task TASK_LEVEL_TRIGGER_INTERRUPT(input [7:0] data);
+    begin
+        #(20 * 0);
+        level_triggered_config = 1'b1;
+        #(20 * 1);
+        interrupt_request_pin   = data;
+        #(20 * 1);
+        clear_interrupt_request = data;
+        #(20 * 1);
+        clear_interrupt_request = 8'b00000000;
+        #(20 * 1);
+    end
+    endtask
+
+    //
+    // Task : Edge trigger interrupt
+    //
+    task TASK_EDGE_TRIGGER_INTERRUPT(input [7:0] data);
+    begin
+        #(20 * 0);
+        level_triggered_config = 1'b0;
+        #(20 * 1);
+        interrupt_request_pin   = 8'b00000000;
+        #(20 * 1);
+        interrupt_request_pin   = data;
+        #(20 * 1);
+        clear_interrupt_request = data;
+        #(20 * 1);
+        clear_interrupt_request = 8'b00000000;
+        #(20 * 1);
+    end
+    endtask
+
+    //
+    // Task : Clear interrupt request
+    //
+    task TASK_CLEAR_INTERRUPT_REQUEST(input [7:0] data);
+    begin
+        #(20 * 0);
+        clear_interrupt_request = data;
+        #(20 * 1);
+    end
+    endtask
+
+    //
+    // Task : Interrupt test
+    //
+    task TASK_INTERRUPT_TEST();
+    begin
+        #(20 * 0);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000001);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000010);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000100);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00001000);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00010000);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00100000);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b01000000);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b10000000);
+        TASK_LEVEL_TRIGGER_INTERRUPT(8'b00000000);
+
+        TASK_CLEAR_INTERRUPT_REQUEST(8'b11111111);
+        TASK_CLEAR_INTERRUPT_REQUEST(8'b00000000);
+
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b10000000);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b01000000);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b00100000);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b00010000);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b00001000);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b00000100);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b00000010);
+        TASK_EDGE_TRIGGER_INTERRUPT(8'b00000001);
+
+        TASK_CLEAR_INTERRUPT_REQUEST(8'b11111111);
+        TASK_CLEAR_INTERRUPT_REQUEST(8'b00000000);
+
+        #(20 * 1);
+    end
+    endtask
+
+
+    //
+    // Test pattern
+    //
+    initial begin
+        TASK_INIT();
+
+        TASK_INTERRUPT_TEST();
+
+        freeze = 1'b1;
+
+        TASK_INTERRUPT_TEST();
+
+        #(20 * 1);
+        // End of simulation
+`ifdef IVERILOG
+        $finish;
+`elsif  MODELSIM
+        $stop;
+`else
+        $finish;
+`endif
+    end
 endmodule
+
+
